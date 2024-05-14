c777a4215ebec9296e66b6e254953726a2a0109f
unknown <elhadjoumartall10@gmail.com>
2024-05-14 15:13:18 +0200

    Modification du code

---
+++ connexion.php
@@ -0,0 +1,3 @@
+<!-- Inscription -->
+<?php
+    $bdd = new PDO('mysql:host=localhost;dbname=projetweb;charset=utf8', 'root', '');
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const logreBox = document.querySelector('logreg-box');
 const loginLink = document.querySelector('login-link');
-const registerLink = document.querySelector('registerLink');
+const registerLink = document.querySelector('register-link');
 
 registerLink.addEventListener('click', () => {
     logreBox.classList('active');
